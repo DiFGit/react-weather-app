d3aeb85079347eea98acbb340cbd975e43cdeba3
DiFGit <55925077+DiFGit@users.noreply.github.com>
2019-11-30 13:36:56 +0000

    improved units conversor style
    by using useState on classeNames

--- src/WeatherUnits.js
+++ src/WeatherUnits.js
@@ -4,34 +4,34 @@ import "./WeatherUnits.css";
 export default function WeatherUnits(props) {
   let [temperature, setTemperature] = useState(props.temperature);
   let [conversion, setConversion] = useState(false);
+  let [celsius, setCelsius] = useState("active btn btn - lg units celsius");
+  let [fahrenheit, setFahrenheit] = useState(
+    "inactive btn btn-lg units fahrenheit"
+  );
   function displayImperial(event) {
     event.preventDefault();
     setTemperature((props.temperature * 9) / 5 + 32);
     setConversion(true);
+    setFahrenheit("active btn btn - lg units celsius");
+    setCelsius("inactive btn btn-lg units fahrenheit");
   }
 
   function displayMetrics(event) {
     event.preventDefault();
     setTemperature(props.temperature);
+    setFahrenheit("inactive btn btn - lg units celsius");
+    setCelsius("active btn btn-lg units fahrenheit");
   }
 
   if (conversion.true) {
     return (
       <div className="units-links">
         <h5>{temperature}</h5>
-        <a
-          href="/"
-          onClick={displayMetrics}
-          className="inactive btn btn-lg units celsius"
-        >
+        <a href="/" onClick={displayMetrics} className={celsius}>
           C{" "}
         </a>
         |
-        <a
-          href="/"
-          onClick={displayImperial}
-          className="active btn btn-lg units fahrenheit"
-        >
+        <a href="/" onClick={displayImperial} className={fahrenheit}>
           F{" "}
         </a>
       </div>
@@ -40,19 +40,11 @@ export default function WeatherUnits(props) {
     return (
       <div className="units-links">
         <h5>{temperature}</h5>
-        <a
-          href="/"
-          onClick={displayMetrics}
-          className="active btn btn-lg units celsius"
-        >
+        <a href="/" onClick={displayMetrics} className={celsius}>
           C{" "}
         </a>
         |
-        <a
-          href="/"
-          onClick={displayImperial}
-          className="inactive btn btn-lg units fahrenheit"
-        >
+        <a href="/" onClick={displayImperial} className={fahrenheit}>
           F{" "}
         </a>
       </div>
