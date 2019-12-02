aad86552cbbecbf22f32d18566cf181903d32aeb
DiFGit <55925077+DiFGit@users.noreply.github.com>
2019-12-02 18:53:22 +0000

    added fahrenheit conversion to forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -25,7 +25,47 @@ export default function Forecast(props) {
     setLoaded(true);
   }
 
-  if (loaded && forecast.city.name === props.city) {
+  if (
+    loaded &&
+    forecast.city.name === props.city &&
+    props.units === "imperial"
+  ) {
+    let forecast24h = [7, 15, 23, 31, 39];
+    return (
+      <footer className="forecastBox">
+        <div className="row justify-content-center forecast">
+          {forecast24h.map(index => {
+            return (
+              <div className="col-auto">
+                {
+                  <span>
+                    <small key={index.dt}>
+                      {formatForecastDay(
+                        forecast.list[index].dt * 1000 - timezone * 1000
+                      )}
+                    </small>
+                    <img
+                      key={index.icon}
+                      src={`http://openweathermap.org/img/wn/${forecast.list[index].weather[0].icon}@2x.png`}
+                      className="forecastIcon"
+                      alt=""
+                    />
+                    <small key={index.temp}>
+                      {Math.round(
+                        (forecast.list[index].main.temp * 9) / 5 + 32
+                      )}
+                      º
+                    </small>
+                  </span>
+                }
+              </div>
+            );
+          })}
+        </div>
+      </footer>
+    );
+  }
+  if (loaded && forecast.city.name === props.city && props.units === "metric") {
     let forecast24h = [7, 15, 23, 31, 39];
     return (
       <footer className="forecastBox">
